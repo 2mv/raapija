946de0ffa36330aca262e23074d5cd289fa186bd
Teemu Pääkkönen <teemu@flockler.com>
2016-01-30 20:30:21 +0200

    use utf-8 encoding for last transactions file

--- last_transactions_parser.py
+++ last_transactions_parser.py
@@ -10,7 +10,7 @@ class LastTransactionsParser:
   @staticmethod
   def read():
     try:
-      with open(LastTransactionsParser.LAST_TRANSACTIONS_FILENAME, 'r') as csvfile:
+      with open(LastTransactionsParser.LAST_TRANSACTIONS_FILENAME, 'r', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         return [transaction for transaction in reader]
     except FileNotFoundError:
@@ -18,7 +18,7 @@ class LastTransactionsParser:
 
   @staticmethod
   def write(transactions):
-    with open(LastTransactionsParser.LAST_TRANSACTIONS_FILENAME, 'w') as csvfile:
+    with open(LastTransactionsParser.LAST_TRANSACTIONS_FILENAME, 'w', encoding='utf-8') as csvfile:
       csv_fieldnames = transactions[0].keys()
       writer = csv.DictWriter(csvfile, csv_fieldnames)
       writer.writeheader()
